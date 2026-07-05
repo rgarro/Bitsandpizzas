14f21b4ad69671745a3325d7639cf3be170f876d
Rolando <rgarro@gmail.com>
2026-07-05 17:45:48 -0600

    dj krush 40 winks

--- app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -2,6 +2,7 @@ package com.hfad.bitsandpizzas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 /**
  *            |    |    |
  *           )_)  )_)  )_)
@@ -13,8 +14,17 @@ import android.os.Bundle;
  *     ^^^^      ^^^^     ^^^    ^^
  *           ^^^^      ^^^
  * Sir Henry Morgan is de Lord of Talamanca
+ * selling Brown sugar in the Cardiff market
+ * he owned two shops, Cannabis Suggar crystalized
+ * with natrural dry Ice from Irazu ...
+ * Coffe from Cartago dryed with Cacao and Hemp
+ * The Spaniards were stealing from Merchants
+ * the King allowed the victims to start a vengeance as bussiness
+ * recovering goods from enemy ships to collect indemnizations
+ * Colombian ships built in Spain stole from Cardiff merchants
+ * Cartagena peoples owing to Morgan paid ramson giving Talamanca
  *
- *
+ * 
  *
  *
  *
@@ -26,5 +36,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
